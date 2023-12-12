4fd350a6dc1230cc910b78e94e4f20287e4c049f
xutianyi <xutianyi1999@live.com>
2023-12-12 15:48:24 +0800

    update

--- examples/build_tree.rs
+++ examples/build_tree.rs
@@ -9,6 +9,6 @@ fn main() {
     let out_path = PathBuf::from(format!("{}_treed", in_path));
     let in_path = PathBuf::from(in_path);
 
-    let md = std::fs::metadata(in_path).unwrap();
+    let md = std::fs::metadata(&in_path).unwrap();
     treed_gpu::build_treed(&in_path, &out_path, &mut vec![0u8; md.len() as usize * 2 - 32]).unwrap();
 }
